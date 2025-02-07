8c5573d0731508a84ba8253c5967e2dd23047ca4
Octavio1083 <octavioozaeta8220@gmail.com>
2025-02-07 16:47:50 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+// funcion que no devuelve nada
+// funcion que saluda
+void saludar(){
+  print("Hola Mundo");
+}
+
+void escribirnombre(){
+  print("Octavio Ozaeta");
+}
+
+void main(){
+  // Octavio Ozaeta Mat: 22308051281083
+  // Llamar a la funcion saludar
+  print("Octavio Ozaeta Mat: 22308051281083");
+  print("Llamando a la funcion saludar");
+  saludar();
+  print("Llamando a la funcion escribirnombre");
+  escribirnombre();
+}
